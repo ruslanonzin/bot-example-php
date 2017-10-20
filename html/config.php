5d2637a3ebfd817760cf48080966a18f7c5abd29
ruslanonzin <30985267+ruslanonzin@users.noreply.github.com>
2017-10-21 00:38:58 +0400

    Update config.php

--- html/config.php
+++ html/config.php
@@ -6,5 +6,5 @@ define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/images');
 define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
 
 define('CALLBACK_API_CONFIRMATION_TOKEN', 'bb112c41'); //Строка для подтверждения адреса сервера из настроек Callback API
-define('VK_API_ACCESS_TOKEN', '016cb55230eb83e34f620389s149345d975bc12e2fb6f30e0a833a51d345d0d123c3dc0abc1c864036sdf989fb8345'); //Ключ доступа сообщества
-define('YANDEX_API_KEY', '30e3213440-61233-1294-b3415-471212369886'); //Ключ для доступа к Yandex Speech Kit
+define('VK_API_ACCESS_TOKEN', '2d3e6c697462768120f9ecbf01d1e3f5e4572001e7f5aedaf6d20a70f5cc2143a24d1e1e93018cf989645'); //Ключ доступа сообщества
+define('YANDEX_API_KEY', '801314bd-677c-40b7-acfd-531153d31c1d'); //Ключ для доступа к Yandex Speech Kit
